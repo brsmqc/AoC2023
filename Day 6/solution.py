5cee13f3fb97d5cc614dfb266bf4e6fc9a383267
Barry Still <brsmqc@gmail.com>
2023-12-06 10:01:47 -0300

    add calculate winning possibilities

--- Day 6/solution.py
+++ Day 6/solution.py
@@ -1,6 +1,6 @@
 import re
 
-INPUT = "Day 6\\test_input.txt"
+INPUT = "Day 6\\input.txt"
 
 def get_data(data:str) -> list:
     input = ""
@@ -14,6 +14,17 @@ def get_data(data:str) -> list:
         data_list.append(times)
     return data_list
 
+def calc_num_of_winning_times(time:int, distance:int) -> int:
+    winners = 0
+    for milisec in range(time):
+        if milisec * (time - milisec) > distance:
+            winners += 1
+    print(winners)
+    return winners
+
 if __name__ == "__main__":
     races = get_data(INPUT)
-    print(races)
+    total = 1
+    for x in range(len(races[0])):
+        total *= calc_num_of_winning_times(races[0][x], races[1][x])
+    print(f"total possible ways: {total}")
