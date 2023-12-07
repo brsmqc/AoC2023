401147065ea40b2a7acc7ffb703ba2739e24732a
Barry Still <brsmqc@gmail.com>
2023-12-07 08:51:17 -0300

    add find_types function

--- Day 7/solution.py
+++ Day 7/solution.py
@@ -1,4 +1,5 @@
 INPUT = "Day 7\\test_input.txt"
+ORDER = dict((key, idx) for idx, key in enumerate(["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]))
 
 
 def get_data(data: str) -> list:
@@ -16,19 +17,48 @@ def make_hands_and_bids(hands: list) -> list:
         hands_list.append([hand_str[0], int(hand_str[1])])
     return hands_list
 
+def sort_hand(hand:list) -> None:
+    sorted_hand = sorted(hand[0], key=ORDER.get)
+    # print(sorted_hand)
+    hand[0] = "".join(sorted_hand)
 
-def find_rank(hand: list) -> None:
+
+def find_type(hand: list) -> None:
     chars = {}
-    for char in hand:
+    for char in hand[0]:
         if char in chars.keys():
             chars[char] += 1
         else:
             chars[char] = 1
+    # print(chars)
+    types = []
+    for value in chars.values():
+        types.append(value)
+    types.sort(reverse=True)
+    print(types)
+    if types[0] == 5:
+        hand.append(1)
+    elif types[0] == 4:
+        hand.append(2)
+    elif types[0] == 3 and types[1] == 2:
+        hand.append(3)
+    elif types[0] == 3:
+        hand.append(4)
+    elif types[0] == 2 and types [1] == 2:
+        hand.append(5)
+    elif types[0] == 2:
+        hand.append(6)
+    else:
+        hand.append(7)
+    
 
 
 if __name__ == "__main__":
     raw_list = get_data(INPUT)
     hands = make_hands_and_bids(raw_list)
     for hand in hands:
-        find_rank(hand[0])
-    # print(hands)
+        sort_hand(hand)
+        find_type(hand)
+    hands.sort(key= lambda hand: hand[2], reverse=True)
+    print(hands)
+
