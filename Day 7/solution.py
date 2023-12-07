f21d8d0dfa4fba2fce104d6fdfc06608be38cc29
Barry Still <brsmqc@gmail.com>
2023-12-07 12:24:39 -0300

    added sorting and ranking

--- Day 7/solution.py
+++ Day 7/solution.py
@@ -1,3 +1,5 @@
+from operator import itemgetter
+
 INPUT = "Day 7\\test_input.txt"
 ORDER = dict((key, idx) for idx, key in enumerate(["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]))
 
@@ -35,7 +37,7 @@ def find_type(hand: list) -> None:
     for value in chars.values():
         types.append(value)
     types.sort(reverse=True)
-    print(types)
+    # print(types)
     if types[0] == 5:
         hand.append(1)
     elif types[0] == 4:
@@ -52,6 +54,60 @@ def find_type(hand: list) -> None:
         hand.append(7)
     
 
+def find_rank(hands:list) -> list:
+    type_1 = [hand for hand in hands if hand[2] == 1]
+    type_2 = [hand for hand in hands if hand[2] == 2]
+    type_3 = [hand for hand in hands if hand[2] == 3]
+    type_4 = [hand for hand in hands if hand[2] == 4]
+    type_5 = [hand for hand in hands if hand[2] == 5]
+    type_6 = [hand for hand in hands if hand[2] == 6]
+    type_7 = [hand for hand in hands if hand[2] == 7]
+    
+    # sort by first letter
+    for type in [type_1, type_2, type_3, type_4, type_5, type_6, type_7]:
+        if type != []:
+            type = sort_hands(type, 0)
+    combined_list = type_7 + type_6 + type_5 + type_4 + type_3 + type_2 + type_1
+    return combined_list
+
+def sort_hands(hands_list:list, letter_pos:int) -> list:
+    # sort by the letter in question
+    hands_list = sorted(hands_list, key=lambda x: ORDER.get(x[0][letter_pos]))
+    
+    sorted_hands_list = []
+    if hands_list == []:
+        return
+    temp_list = []
+    for idx, hand in enumerate(hands_list):
+        # last in list
+        if hand == hands_list[len(hands_list)-1]:
+            if hand in temp_list:
+                pass
+            else:
+                sorted_hands_list.append(hand)
+        # first iteration only
+        elif hand == hands_list[0]:
+            if hand[0][letter_pos] == hands_list[idx+1][0][letter_pos]:
+                temp_list.append(hand)
+                temp_list.append(hands_list[idx+1])
+            else:
+                sorted_hands_list.append(hand)
+        # every time after that
+        elif hand[0][letter_pos] == hands_list[idx+1][0][letter_pos]:
+                    temp_list.append(hands_list[idx+1])
+        else:
+            sorted_hands_list.append(hand)
+    if temp_list != []:
+        sorted_hands_list = [hand for hand in sort_hands(temp_list, letter_pos+1) if hand != None]
+    return sorted_hands_list
+
+
+def caulcate_winnings(ranked_list:list) -> int:
+    total = 0
+    for rank, hand in enumerate(ranked_list):
+        total += hand[1] * (rank + 1)
+    return total
+
 
 if __name__ == "__main__":
     raw_list = get_data(INPUT)
@@ -59,6 +115,9 @@ if __name__ == "__main__":
     for hand in hands:
         sort_hand(hand)
         find_type(hand)
-    hands.sort(key= lambda hand: hand[2], reverse=True)
-    print(hands)
+    hands.sort(key= lambda hand: hand[2])
+    ranked = find_rank(hands)
+    winnings = caulcate_winnings(ranked)
+    print(ranked)
+    print(winnings)
 
