5f42f7e39c57c644c15cb9e508467db8754d2462
Barry Still <brsmqc@gmail.com>
2023-12-11 09:10:21 -0300

    add find_galaxies function

--- Day 11/solution.py
+++ Day 11/solution.py
@@ -19,7 +19,22 @@ def expand_universe(universe: list) -> list:
             new_universe.append(line)
     return new_universe
 
+def find_galaxies(universe: list) -> list:
+    count = 1
+    galaxies = []
+    for idx_y, line in enumerate(universe):
+        if "#" in line:
+            for idx_x, char in enumerate(line):
+                if char == "#":
+                    galaxies.append([count, (idx_x, idx_y)])
+                    count += 1
+    return galaxies
+
 
 if __name__ == "__main__":
     universe = get_data(INPUT)
     expanded_universe = expand_universe(universe)
+    galaxy_list = find_galaxies(expanded_universe)
+
+    for galaxy in galaxy_list:
+        print(galaxy)
