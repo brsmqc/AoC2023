02bb9e4d0807510df32ad026a44379b34aaa1b91
Barry Still <brsmqc@gmail.com>
2023-12-13 09:48:59 -0300

    Add day 13

---
+++ Day 13/solution.py
@@ -0,0 +1,9 @@
+INPUT = "Day 13\\test_input.txt"
+
+
+def get_data(data: str) -> list:
+    temp = ""
+    with open(data) as file:
+        temp = file.read()
+    return_list = temp.split("\n")
+    return return_list
