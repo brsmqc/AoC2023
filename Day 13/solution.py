cc3388ec4bb7bef25a07632ee3b8df6942567dab
Barry Still <brsmqc@gmail.com>
2023-12-13 13:48:45 -0300

    Add make_2d_lists function
    Formats the data into mutliple 2d lists and returns the list of lists.

--- Day 13/solution.py
+++ Day 13/solution.py
@@ -7,3 +7,20 @@ def get_data(data: str) -> list:
         temp = file.read()
     return_list = temp.split("\n")
     return return_list
+
+def make_2d_lists(data: list) -> list:
+    patterns = []
+    temp_pattern = []
+    for idx, line in enumerate(data):
+        if line == "" or idx == len(data)-1:
+            patterns.append([item for item in temp_pattern])
+            temp_pattern.clear()
+        else:
+            temp_pattern.append(line)
+    return patterns
+
+if __name__ == "__main__":
+    data = get_data(INPUT)
+    patterns = make_2d_lists(data)
+    for pattern in patterns:
+        print(pattern)
